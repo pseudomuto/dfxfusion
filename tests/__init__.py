a474ca820afd6c39ae8c27a0bd55bca1c4f2133c
David Muto (pseudomuto) <david.muto@gmail.com>
2022-02-21 11:23:36 -0500

    Ensure project root in sys.path for tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,6 @@
 import os.path
 import sys
 
-# add lib to sys.path for testing
+# add root and lib to sys.path for testing
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'lib'))
