b6aeb564ea0aa309448ff83069cb3e4aa7f14aac
David Muto (pseudomuto) <david.muto@gmail.com>
2022-02-21 11:25:48 -0500

    Add support for submodules in CI

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,5 @@
 import os.path
 import sys
 
-# add root and lib to sys.path for testing
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+# add lib to sys.path for testing
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'lib'))
